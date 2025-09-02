cbb9e78fb0cc6728f51fc4b5c37f3cf52bc6a7df
clemgi0 <clement.giraudon2@gmail.com>
2025-09-02 14:36:53 -0500

    test auto-deploys

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hola desde Next.js, by Clemgi 🚀</h1>;
+  return <h1>Hola desde Next.js, by Clemgi, moi quoi 🚀</h1>;
 }
